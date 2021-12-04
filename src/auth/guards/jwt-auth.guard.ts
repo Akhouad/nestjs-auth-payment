57f8881f8691fe805bddd99ffb58c970cd6a644d
Amine Akhouad <akhouad@yahoo.fr>
2021-12-04 16:24:26 +0100

    Add skipAuth decorator

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,5 +1,28 @@
-import { Injectable } from "@nestjs/common";
+import { ExecutionContext, Injectable, SetMetadata } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
 import { AuthGuard } from "@nestjs/passport";
 
+export const IS_PUBLIC_KEY = 'isPublic';
+export const SkiAuth = () => SetMetadata(IS_PUBLIC_KEY, true);
+
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(
+    private _reflector: Reflector,
+  ) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext) {
+    const isPublic = this._reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass()
+    ]);
+    
+    if (isPublic) {
+      return true;
+    }
+
+    return super.canActivate(context);
+  }
+}
