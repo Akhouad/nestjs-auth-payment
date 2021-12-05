2853ee80788cba6558c0182c44930fb622a98fb3
Amine Akhouad <akhouad@yahoo.fr>
2021-12-05 12:38:25 +0100

    init with database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { User } from './users/user.entity';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     AuthModule, 
     UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root', // TODO get from env file
+      password: '', // TODO get from env file
+      database: 'joboodle', // TODO get from env file
+      entities: [User],
+      synchronize: true,
+    }),
   ],
   controllers: [
     AppController,
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,24 +1,23 @@
 import { Injectable } from '@nestjs/common';
-
-// This should be a real class/interface representing a user entity
-export type User = any;
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
 export class UsersService {
-  private readonly users = [
-    {
-      userId: 1,
-      username: 'john',
-      password: 'changeme',
-    },
-    {
-      userId: 2,
-      username: 'maria',
-      password: 'guess',
-    },
-  ];
+  constructor(
+    @InjectRepository(User) private _userRepository: Repository<User>,
+  ) {}
 
-  async findOne(username: string): Promise<User | undefined> {
-    return this.users.find(user => user.username === username);
+  findAll(): Promise<User[]> {
+    return this._userRepository.find();
+  }
+  
+  findOne(id: number): Promise<User> {
+    return this._userRepository.findOne(id);
+  }
+
+  async remove(id: number): Promise<void> {
+    await this._userRepository.delete(id);
   }
 }
